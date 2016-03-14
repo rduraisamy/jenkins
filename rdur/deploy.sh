b355157cc4f5f1f16ed844b4331950fdcb3a418c
Roshan Duraisamy <rduraisamy@Roshans-MacBook-Pro.local>
2016-03-13 17:38:49 -0700

    added deploy.sh

---
+++ rdur/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x 
+ps -efwww|grep jenkins |grep -v grep|awk '{print $2} > ./mypid.txt
+
+
+if [ -s ./mypid.txt ]; then 
+   kill -9 `cat ./mypid.txt`; 
+fi; 
+
+rm -f ./mypid.txt
+
+nohup java -jar jenkins.war &
--- rdur/pipeline.groovy
+++ rdur/pipeline.groovy
@@ -22,16 +22,18 @@ node('master') {
   //sh "${mvnHome}/bin/mvn -Plight-test install"
  
   archive 'war/target/jenkins.war'
+  archive 'rdur/deploy.sh'
 }
 
 node ('ubuntu-server') {
    stage 'Deploy'
 
-   sh "ps -efwww|grep jenkins |grep -v grep|awk '{print \$2}' > ./mypid.txt"
+//   sh "ps -efwww|grep jenkins |grep -v grep|awk '{print \$2}' > ./mypid.txt"
 
-   sh "if [ -s ./mypid.txt ]; then kill -9 `cat ./mypid.txt`; fi; rm -f ./mypid.txt"
-   unarchive mapping: ['war/target/jenkins.war' : '.']
-   sh "nohup java -jar jenkins.war &"
+//   sh "if [ -s ./mypid.txt ]; then kill -9 `cat ./mypid.txt`; fi; rm -f ./mypid.txt"
+   unarchive mapping: ['war/target/jenkins.war' : '.', 'rdur/deploy.sh' : '.']
+   sh "bash deploy.sh"
+//   sh "nohup java -jar jenkins.war &"
 
 }
 
