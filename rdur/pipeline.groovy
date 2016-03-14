656956547d752db4f81d16b127806b8990d18245
Roshan Duraisamy <rduraisamy@Roshans-MacBook-Pro.local>
2016-03-13 23:57:27 -0700

    Productionized the demo a little

--- rdur/pipeline.groovy
+++ rdur/pipeline.groovy
@@ -6,36 +6,40 @@ node('master') {
   stage 'Checkout'
 
   // Get some code from a GitHub repository
+  // Note that there are many more parameters that could be used here
   git url: 'https://github.com/rduraisamy/jenkins.git'
 
   // Get the maven tool.
-  // ** NOTE: This 'M3' maven tool must be configured
-  // **       in the global configuration.           
   def mvnHome = tool 'M3'
 
   // Mark the code build 'stage'....
   stage 'Build'
+
   // Run the maven build
   sh "${mvnHome}/bin/mvn install -pl war -am -DskipTests"
-  
+ 
+  // Run the maven tests 
   stage 'Test'
   sh "${mvnHome}/bin/mvn -Plight-test install"
  
+  // Archive the artifacts that will be used for deployment
   archive 'war/target/jenkins.war'
-  archive 'rdur/deploy.sh'
 }
 
 node ('ubuntu-server') {
    stage 'Deploy'
 
-   //checking if there is already Jenkins instance running 
+   // Load the artifacts that we are going to deploy
    unarchive mapping: ['war/target/jenkins.war' : '/home/rdur/jenkins.war']
+
+   // remove the current production jenkins install
    sh "rm -f mypid"
    sh "ps -efwww  |grep -v grep|grep  \"/home/rdur/jenkins.war\"|awk {'print \$2'}>mypid"
  
    sh "if [ -s mypid ]; then kill -9  `cat mypid`; fi"
    sh "sleep 10"
-   
+
+   // install the latest jenkins   
    sh "nohup java -jar /home/rdur/jenkins.war &"
    sh "ls"
 
